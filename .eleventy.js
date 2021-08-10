27f4ff75c2832a7226daf7be10acebb6ee1e3dab
Marco Romeny <marco@mimecom.net>
2021-08-11 00:21:41 +0200

    added reveal files

--- .eleventy.js
+++ .eleventy.js
@@ -73,8 +73,10 @@ module.exports = function (eleventyConfig) {
     "./_tmp/static/css/style.css": "./static/css/style.css",
     "./src/admin/config.yml": "./admin/config.yml",
     "./node_modules/alpinejs/dist/alpine.js": "./static/js/alpine.js",
-    "./node_modules/prismjs/themes/prism-tomorrow.css":
-      "./static/css/prism-tomorrow.css",
+    "./node_modules/prismjs/themes/prism-tomorrow.css": "./static/css/prism-tomorrow.css",
+    "./node_modules/reveal.js/dist/reveal.js":"./static/js/reveal.js",
+    "./node_modules/reveal.js/dist/reset.css":"./static/css/reset.css",
+    "./node_modules/reveal.js/dist/reveal.css":"./static/css/reveal.css",
   });
 
   // Copy Image Folder to /_site
